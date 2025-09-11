4c2c40b98f78a4bfd73a0eb67238cac2d805d4ee
StetsYaroslavl <stetsyar2011@gmail.com>
2025-09-11 13:54:27 +0300

    njkjhj

--- lesson 1/task 2.py
+++ lesson 1/task 2.py
@@ -1,10 +1,12 @@
 a1 = int(input("Введіть число: "))
 a = a1
 l = []
-b = 1
-while a // b != 0:
-    c = a // b
+b = 10
+while b // a != 0:
+    c1 = b // a
+    c = b // c1
+    a = a - c
     if c // 2 : 
         l.append(c)
     b *= 10
-
+print("l ", l)
